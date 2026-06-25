d115a8ca229fe85502f98e0e6b8c3b9dc0ca6bf5
prachi23patel <ppptl237@gmail.com>
2026-06-25 19:21:11 +0530

    app/models/rooms.py

--- app/models/rooms.py
+++ app/models/rooms.py
@@ -9,7 +9,7 @@ class Rooms(Base):
     __tablename__ = "Rooms"
 
     room_id = Column(String, primary_key=True)
-    room_name = Column(String , unique=True , nullable=False)
+    room_name = Column(String , nullable=False)
     owner_id = Column(UUID(as_uuid=True) , nullable=False)
     room_password = Column(String , nullable=False)
     created_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
