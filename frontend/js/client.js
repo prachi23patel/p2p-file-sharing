40c50728d4c5530de1360b290bc6a79faca00b82
prachi23patel <ppptl237@gmail.com>
2026-06-26 08:42:15 +0530

    frontend/js/client.js

--- frontend/js/client.js
+++ frontend/js/client.js
@@ -178,7 +178,7 @@ async function startAnswerer(webso , peerId) {
 // SIGNALING CONNECTION
 // ===========================
 function connectToSignaling(role) {
-    const wsUrl = `wss://peer-to-peer-file-sharing-yvei.vercel.app/ws/${roomId}`;
+    const wsUrl = `wss:///peer-to-peer-file-sharing-production-d1a0.up.railway.app/ws/${roomId}`;
     // console.log(`🔌 Connecting to signaling server: ${wsUrl}`);
     myId = sessionStorage.getItem('myId');
     ws = new WebSocket(wsUrl);
